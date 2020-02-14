e5b42d183d164ba09cae90d39cb973132e7c2f9e
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-02-14 11:39:07 -0600

    hashtables/ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,17 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+
+    for i in range(length):
+        variable_weight = hash_table_retrieve(ht, limit - weights[i])
+
+        if variable_weight:
+            return (variable_weight, i)
 
     return None
 
